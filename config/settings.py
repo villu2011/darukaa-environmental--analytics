11af1b0e99f990044d47153db498a630e5abdde6
your Name <your-email@example.com>
2026-09-19 18:51:14 +0530

    secure deployment secrets

--- config/settings.py
+++ config/settings.py
@@ -20,8 +20,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-@sd972^&+hvt&e%2c4)n_7%waer2ksm(jm-y5qo!0t($d!2gvi'
-
+import os
+SECRET_KEY = os.environ.get("SECRET_KEY","dev-only-secret-key")  # Replace with your actual secret key in production
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
